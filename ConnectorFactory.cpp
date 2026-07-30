1fbb2ec748cb014ce8ee582002d5ce020aaaa2c5
Chloe Larsen <chloelarsen72@gmail.com>
2026-07-30 17:51:46 +0200

    Implementing ConnectorFactory

--- ConnectorFactory.cpp
+++ ConnectorFactory.cpp
@@ -1,11 +1,20 @@
 #include "ConnectorFactory.h"
 
-Connector *ConnectorFactory::createConnector()
-{
-
-}
-
 ConnectorFactory::~ConnectorFactory()
 {
+}
 
+Connector *PostgresFactory::createConnector()
+{
+    return new PostgresConnector();
+}
+
+Connector *RestApiFactory::createConnector()
+{
+    return new RestApiConnector();
+}
+
+Connector *CsvFactory::createConnector()
+{
+    return new CsvConnector();
 }
