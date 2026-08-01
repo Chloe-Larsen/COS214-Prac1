91e2b999965fc0f993793d95f64c25daf398605c
cablexd <cableplays1912@gmail.com>
2026-08-01 19:07:34 +0200

    complete task 4

--- CheckpointManager.cpp
+++ CheckpointManager.cpp
@@ -4,14 +4,25 @@
 */
 #include "CheckpointManager.h"
 
-void CheckpointManager::save(RunCheckpoint *)
+void CheckpointManager::save(RunCheckpoint *checkpoint)
 {
+    history.push_back(checkpoint);
 }
 
 RunCheckpoint *CheckpointManager::undo()
 {
+    if (history.empty())
+        return nullptr;
+
+    RunCheckpoint *checkpoint = history.back();
+    history.pop_back();
+    return checkpoint;
 }
 
 CheckpointManager::~CheckpointManager()
 {
+    for (RunCheckpoint *checkpoint : history)
+    {
+        delete checkpoint;
+    }
 }
--- RunCheckpoint.h
+++ RunCheckpoint.h
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <map>
 
+
+
 class RunCheckpoint
 {
 private:
